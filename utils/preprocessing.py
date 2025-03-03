2dae18b2b8ada4e459f47ac64175968cf473f370
Markus Kristoffer Sibbesen <mksi@itu.dk>
2025-03-03 14:16:28 +0100

    test train split in preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,6 +1,7 @@
 from classes.datahandling import TextClassificationDataset
-
-
+from torch import Generator
+from torch.utils.data import random_split
+from pathlib import Path
 
 
 def load_txt_data(
@@ -11,9 +12,7 @@ def load_txt_data(
     
     Args:
         **kwargs: Language code to file path mappings where each keyword argument name represents a language code (e.g., 'en', 'da', 'sv') and each value is the corresponding file path.
-        
-        file_extention: str in {'txt', 'tml'}
-        
+        file_extention: str in {'txt', 'tml'}   
         min_sentence_length: int for min amount of tokens in sentences loaded.
     
     Returns:
@@ -56,6 +55,48 @@ def filter_short_sentences(
 
     
 
+def split_text_data(
+    ds: TextClassificationDataset,
+    out_folder: str,
+    split: tuple = (0.9, 0.1),
+    seed: int = 42):
+    '''
+    Splits text data into train and test
+
+    Args:
+        ds: TextClassificationDataset
+        out_folder: str the folder for the test and train directories
+        split: a tuple of the form (float, float), summing to 1. (0.9, 0.1) means 90% in train and 10% in test
+        seed: int random seed
+
+
+    Returns:
+        None (just created files)
+    '''
+
+
+    out_folder = Path(out_folder)
+    labels = ds.label_map.keys()
+
+    for label in labels:
+        label_subset = ds.filter_by_language(label)
+
+        train, test = random_split(
+            dataset=label_subset, 
+            lengths=split,
+            generator=Generator().manual_seed(seed))
+
+        train_path = out_folder / 'train' / f'{label}.txt'
+        train_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(str(train_path), 'w', encoding='utf-8') as f:
+            for sentence, _ in train:
+                f.write(sentence + '\n')
+
+        test_path = out_folder / 'test' / f'{label}.txt'
+        test_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(str(test_path), 'w', encoding='utf-8') as f:
+            for sentence, _ in test:
+                f.write(sentence + '\n')
 
 
 
