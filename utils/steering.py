66bd17795fe85200bec00a74015a1b9a6b11e4c7
Markus Kristoffer Sibbesen <mksi@itu.dk>
2025-03-07 10:20:53 +0100

    loss under steering

--- utils/steering.py
+++ utils/steering.py
@@ -5,12 +5,13 @@ from classes.hook_manager import HookManager
 def generate_with_steering(
         model,
         tokenizer,
-        layer,
+        layer: int,
         text_prompts: TextClassificationDataset,
         steering_vector: torch.Tensor,
         steering_lambda: int = 1,
         amount_samples: int = 10,
-        cut_off: int = 10) -> list[str]:
+        cut_off: int = 10
+    ) -> list[str]:
     '''
     Generates text from a set of prompts. The prompts will be cut up after a certain amount of tokens, and continued by the model under steering
 
@@ -54,3 +55,68 @@ def generate_with_steering(
             outputs.append(tokenizer.decode(undecoded_output[0]).replace('\n', '  '))
 
     return outputs
+
+
+def loss_with_steering(
+        model,
+        tokenizer,
+        layer: int,
+        prompt: str,
+        continuation: str,
+        steering_vector: torch.Tensor,
+        steering_lambda: int = 1
+    ) -> float:
+    '''
+    Computes the loss (next-token prediction) for a steered model, given a prompt and a true continuation. Returns only the loss on the continuation.
+
+    Args:
+        model: the torch model
+        tokenizer: its tokenizer
+        layer: the layer of model to attach steering vector
+        prompt: the text prompt, given as context for prediction
+        continuation: the ground truth continuation, used for computing loss
+        steering_vector: the torch.Tensor steering vector
+        steering_lambda: the scalar which will scale the steering vector before it being applied
+
+    Returns:
+        Average loss on continuation
+    '''
+    
+    device = model.parameters().__next__().device
+
+    with HookManager(model) as hook_manager:
+        hook_manager.attach_residual_stream_activation_based_steering_vector(
+            layer=layer,
+            steering_vector=steering_vector,
+            plus=True,
+            scalar=steering_lambda,
+            pre_mlp=False,
+            pythia=False
+        )
+
+        # tokenizes the prompt and the continuation
+        tokenized_prompt = tokenizer(prompt, return_tensors='pt').to(device)
+        tokenized_continuation = tokenizer(continuation, return_tensors='pt').to(device)
+
+        # combines them into one input
+        tokenized_combined = torch.concat(
+            (tokenized_prompt.input_ids, tokenized_continuation.input_ids), 
+            dim=1
+        )
+
+        # computes output from model 
+        output = model(tokenized_combined)
+
+        # gets predictions and labels (need to be shifted by one)
+        shift_logits = output.logits[:, :-1, :]
+        shift_labels = tokenized_combined[:, 1:]
+
+        # computes cross-entropy loss for all tokens individualle
+        loss_fn = torch.nn.CrossEntropyLoss(reduction='none')
+        loss_by_token = loss_fn(shift_logits.reshape(-1, shift_logits.size(-1)), shift_labels.reshape(-1))
+
+        # isolates tokens from continuation and averages their loss
+        loss_continuation = loss_by_token[-tokenized_continuation.input_ids.numel():]
+        avg_loss_continuation = loss_continuation.mean().item()
+
+        return avg_loss_continuation
