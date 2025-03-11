05c8cfb0d0725d2659424280acf6aa024078ab63
Markus Kristoffer Sibbesen <mksi@itu.dk>
2025-03-11 11:20:33 +0100

    driver that runs loss analysis

---
+++ drivers/steering_loss.py
@@ -0,0 +1,41 @@
+import torch
+from pathlib import Path
+
+from utils.probe_confidence_intervals import model_setup
+from utils.preprocessing import load_txt_data
+from utils.steering_vector_analysis import plot_loss_for_steering_vectors
+
+from classes.datahandling import ParallelNSPDataset
+
+def run():
+
+    # loads model
+    model_name  = "AI-Sweden-Models/gpt-sw3-356m"
+    model, tokenizer, device = model_setup(model_name)
+
+    # loads data
+    bible_path = Path('data/bible-da-en.tmx')
+    ds = ParallelNSPDataset.from_tmx(
+        str(bible_path),
+        lan1='da',
+        lan2='en'
+    )
+
+    # loads steering vectors by layer
+    steering_vector_folder = Path('steering_vectors/DA/')
+    num_layers = model.config['hidden_layers']
+    steering_vectors_by_layer = {
+        layer: torch.load(steering_vector_folder + f'combined_steering_vector_layer_{layer}_tensor.pt')
+        for layer in range(num_layers)
+    }
+
+    # plots losses on bible data
+    plot_loss_for_steering_vectors(
+        model,
+        tokenizer,
+        ds,
+        steering_vectors_by_layer,
+        lan1='en',
+        lan2='da',
+        amount_datapoints=100
+    )
