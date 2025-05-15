e9eab93893b218a4abfaa90c7310d95be611d830
Markus Kristoffer Sibbesen <mksi@itu.dk>
2025-05-15 11:01:53 +0200

    extracting and steering with pre layernorm

--- classes/hook_manager.py
+++ classes/hook_manager.py
@@ -27,6 +27,29 @@ class HookManager():
 
         return extracted_output
     
+    def attach_residstream_preln_hook(self, layer, pre_mlp=False, pythia=False):
+        if pre_mlp:
+            if pythia:
+                hookpoint = f'gpt_neox.layers.{layer}.post_attention_layernorm'
+            else:
+                hookpoint = f'transformer.h.{layer}.ln_2'
+        else:
+            if pythia:
+                hookpoint = f'gpt_neox.layers.{layer}.input_layernorm'
+            else:
+                hookpoint = f'transformer.h.{layer}.ln_1'
+        
+        extracted_output = []
+        def residstream_hook(module, input, output):
+            extracted_output.append(input[0].squeeze(0).detach())
+
+        self.hooks.append(
+            self.model.get_submodule(hookpoint).register_forward_hook(residstream_hook)
+        )
+
+        return extracted_output
+    
+    
     def attach_resid_stream_steer_hook(self, layer, steering_vector, scalar, pre_mlp=False, pythia=False):
         if pre_mlp:
             if pythia:
@@ -76,6 +99,33 @@ class HookManager():
                 hookpoint = f'gpt_neox.layers.{layer}.attention'
             else:
                 hookpoint = f'transformer.h.{layer}.attn'
+        
+        def steering_hook(module, input):
+            activation = input[0]
+            if plus:
+                activation = activation + steering_vector * scalar
+            else:
+                activation = activation - steering_vector * scalar
+            return activation
+
+        self.hooks.append(
+            self.model.get_submodule(hookpoint).register_forward_pre_hook(steering_hook)
+        )
+
+    def attach_residual_stream_activation_based_steering_vector_preln(self, layer, steering_vector, plus, scalar = 1, pre_mlp=False, pythia=False):
+        #This is martins' 
+        
+        if pre_mlp:
+            if pythia:
+                hookpoint = f'gpt_neox.layers.{layer}.post_attention_layernorm'
+            else:
+                hookpoint = f'transformer.h.{layer}.ln_2'
+        else:
+            if pythia:
+                hookpoint = f'gpt_neox.layers.{layer}.input_layernorm'
+            else:
+                hookpoint = f'transformer.h.{layer}.ln_1'
+        
         
         def steering_hook(module, input):
             activation = input[0]
