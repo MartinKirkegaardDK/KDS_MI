305247a294604c1d3d9bd35f05a0bed34306b441
Markus Kristoffer Sibbesen <mksi@itu.dk>
2025-06-12 15:38:13 +0200

    correct confidence interval version (accidently deleted the wrong one)

--- src/probe_confidence_intervals.py
+++ src/probe_confidence_intervals.py
@@ -1,29 +1,26 @@
-
-#import seaborn as sns
 from torch.utils.data import DataLoader
+from src.utils.old.preprocessing import load_txt_data
+from src.utils.new.hooking import get_activations as get_activations_new
+from src.utils.new.compatibility import ModelConfig
+from src.utils.old.probe_confidence_intervals import model_setup
+from src.utils.old.probe_confidence_intervals import bootstrap
+from collections import defaultdict
 from pathlib import Path
+import numpy as np
 import json
 
-from src.utils.old.probe_confidence_intervals import model_setup, bootstrap, plot_confidence_intervals,get_activations
-from src.utils.old.probe_confidence_intervals import create_classes_by_layer
-from src.utils.old.preprocessing import load_txt_data
 
-import numpy as np
-from collections import defaultdict
-
-
-def run(model_name, reg_lambdas):
+def idk(model_name,model_name_temp, reg_lambdas):
     """This function runs an entire pipeline that bootstraps, trains and creates confidence intervals showing
-       The probes f1 score on different labels and across layers
-       
-       We bootstrap 10 times
-       Results are saved in this folder: results/data/probe_confidence_intervals/*model_name*_reg_lambda_*reg_lambda*
+        The probes f1 score on different labels and across layers
+        
+        We bootstrap 10 times
+        Results are saved in this folder: results/data/probe_confidence_intervals/*model_name*_reg_lambda_*reg_lambda*
 
     Args:
         model_name (_type_): _description_
         reg_lambdas (_type_): _description_
     """
-
 
     # loads model
     print("Load model")
@@ -31,15 +28,15 @@ def run(model_name, reg_lambdas):
 
 
     # loads data
-    data_folder = Path('data/preprocessed/train')
+    folder = lambda x: f'data/antibiotic/{x}.txt'
     print("Load data")
     ds = load_txt_data(
         file_paths={
-            'da': data_folder / 'da.txt',
-            'en': data_folder / 'en.txt',
-            'sv': data_folder / 'sv.txt',
-            'nb': data_folder / 'nb.txt',
-            'is': data_folder / 'is.txt'
+            'da': folder('da'),
+            'en': folder('en'),
+            'is': folder('is'),
+            'sv': folder('sv'),
+            'nb': folder('nb')
         },
         file_extension='txt'
     )
@@ -49,69 +46,78 @@ def run(model_name, reg_lambdas):
 
     # sets training parameters
     meta_data = {}
-
-    try:
-        meta_data["hidden_size"] = model.config.n_embd
-    except AttributeError:
-        meta_data["hidden_size"] = model.config.hidden_size
-
-    meta_data["hidden_layers"] = model.config.num_hidden_layers
+    meta_data["hidden_size"] = ModelConfig.hidden_size(model)
+    meta_data["hidden_layers"] = ModelConfig.hidden_layers(model)
     meta_data["model_name"] = model_name.split("/")[0]
     meta_data["learning_rate"] = 0.001
-    meta_data["reg_lambda"] = 10
     meta_data["amount_epochs"] = 1
 
 
     # extracts activation from forward passes on data
     # We use hooks to extract the different layer activations that will be used to train our probes
-    
-    print("Extract activations")
-    activation_ds_by_layer = get_activations(meta_data,loader, tokenizer, device, model)
 
+    print("Extract activations")
+    activations = get_activations_new(
+        loader=loader, 
+        model=model,
+        tokenizer=tokenizer,
+        hook_addresses=None,
+        layers=None,
+        max_batches=20,
+        sampling_prob=0.1
+    )
+    
+    #Here we get the different positions that we are testing
+    positions = []
+    for index, (key, val) in enumerate(activations.items()):
+        if index == 6: break
+        positions.append(key.replace("layer.0.",""))
+    
+    
+    d = {}
+    for pos in positions:
+        acts_ds_by_layer = {}
+        for layer in range(meta_data["hidden_layers"]):
+            pos_key = f"layer.{layer}.{pos}"
+            acts_ds_by_layer[layer] = activations[pos_key]
+        d[pos] = acts_ds_by_layer
+    
+    #We extract the amount of labels. 
+    #We just do this for a single position as all of them shares the same labels
     s = set()
     for i in range(meta_data["hidden_layers"]):
-        unique_labels = set(np.array(activation_ds_by_layer[i].labels))
+        unique_labels = set(np.array(acts_ds_by_layer[i].labels))
         [s.add(x) for x in unique_labels]
     number_labels = len(s)
     meta_data["number_labels"] = number_labels
 
 
 
-
-    
     data_output_folder = Path('results/data/probe_confidence_intervals')
 
-    for reg_lambda in reg_lambdas:
-        meta_data['reg_lambda'] = reg_lambda
-
-        # initiates all the probes and corresponding data loaders
-        #print("Initiate classes")
-        #probe_by_layer, act_loader_by_layer = create_classes_by_layer(meta_data, activation_ds_by_layer, device)
-
-
-        # We bootstrap the data and train 10 probes for each layer in order to get "confidence intervals"
-        print("Bootstrap")
-        map_lab = ds.map_label
-        boot = bootstrap(10, meta_data, activation_ds_by_layer, device)
-        d = defaultdict(list)
-        for run in boot:
-            for layer in run.keys():
-                class_accuracies = run[layer].class_accuracies
-                d[layer].append(class_accuracies)
-
-
-
-        # saves data used in plots
+    for pos in positions:
         
-        d['map_label'] = map_lab
-        if "downloaded" in model_name:
-            model_name = model_name.split("/")[-1]
-        reg_lambda_output_file = data_output_folder / f"{model_name.replace('/', '-')}_reg_lambda_{meta_data['reg_lambda']}.json"
-        with open(str(reg_lambda_output_file), 'w') as file:
-            json.dump(d, file, indent=4)
+        acts_ds_by_layer = d[pos]
+        print(acts_ds_by_layer, pos, "hi")
+        for reg_lambda in reg_lambdas:
+            #print()
+            meta_data['reg_lambda'] = reg_lambda
+            boot = bootstrap(10, meta_data, acts_ds_by_layer, device)
+            map_lab = ds.map_label
+
+            
+            d_temp = defaultdict(list)
+            for run in boot:
+                for layer in run.keys():
+                    class_accuracies = run[layer].class_accuracies
+                    d_temp[layer].append(class_accuracies)
 
 
 
-    # saves plots of the confidence intervals
-    # plot_confidence_intervals(d, meta_data, map_lab)
+            # saves data used in plots
+            
+            d_temp['map_label'] = map_lab
 
+            reg_lambda_output_file = data_output_folder / f"{model_name_temp}_{pos}_reg_lambda_{meta_data['reg_lambda']}.json"
+            with open(str(reg_lambda_output_file), 'w') as file:
+                json.dump(d_temp, file, indent=4)
